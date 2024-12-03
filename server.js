69d4b2aef8b527e667236447fb58d695d02fb52f
enyichiaagu <enyichiaagu@gmail.com>
2024-12-03 18:28:48 +0100

    added cookie secrets to sign cookies

--- server.js
+++ server.js
@@ -3,17 +3,44 @@ import path from 'node:path';
 import https from 'node:https';
 import express from 'express';
 import helmet from 'helmet';
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth20';
+import cookieSession from 'cookie-session';
 
 const PORT = 3000;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
+
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; // TODO
@@ -26,15 +53,32 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get('/auth/google', (req, res) => {});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
 
-app.get('/auth/google/callback', (req, res) => {});
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Google called us back!');
+  }
+);
 
 app.get('/auth/logout', (req, res) => {});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value is 42!');
 });
+
+app.get('/failure', (req, res) => res.send('Failed to login'));
 
 app.get('/', (req, res) => {
   return res.sendFile(path.join(import.meta.dirname, 'public', 'index.html'));
